c1d7287a8a0be87bb6eca50a605c701d30e3fb2a
cyasts <824814957@qq.com>
2024-09-14 18:42:36 +0800

    init commit

---
+++ components/menu/index.js
@@ -0,0 +1,49 @@
+// components/menu/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    name: {
+      type:String
+    },
+    list: {
+      type: Array   //  在此定义的是组件对外要开发的属性
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    actived: {
+      type:String
+    }   //data里定义的是在组件里自己使用的私有的数据
+  },
+  //组件初始化完成，触发attached， 默认选中第一个选项
+  attached: function(e) {
+    this.setData({
+      actived: this.data.list[0]
+    })
+    this.triggerEvent("choose", { 
+      name: this.properties.name, 
+      id: this.data.actived 
+    })
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    changetype(e) {
+      console.log(e.currentTarget.dataset.id)
+      this.setData({
+        actived: e.currentTarget.dataset.id
+      })
+      this.triggerEvent("choose", { 
+        name: this.properties.name, 
+        id: e.currentTarget.dataset.id 
+      })
+    }
+  }
+})
